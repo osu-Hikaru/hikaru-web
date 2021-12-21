e108d56fff105109336432082e4708276115a160
WitchOfFrost <unlimited.gamer@gmx.de>
2021-12-21 12:00:34 +0100

    just some html

---
+++ main_scripts.js
@@ -0,0 +1,12 @@
+// Licensed under GPL v3 - Check Repository Root for full License notice.
+// osu!Hikaru, a fully independent osu!Lazer Private Server backend.
+// Copyright (C) 2021 Hikaru Team <copyright@hikaru.pw>
+
+$("#login_menu").on("click", function () {
+  $("#login_menu-expanded").toggle();
+});
+
+$("#user_login").on("submit", function (e) {
+  e.preventDefault();
+  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+});

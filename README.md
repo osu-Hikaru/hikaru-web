ceb0ac57acf1473e5147b252d0f473af3966cfb6
WitchOfFrost <unlimited.gamer@gmx.de>
2021-12-21 09:58:04 +0100

    Added license notice

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Hikaru Web
+
+## License Notice
+
+Licensed under GPL v3 - Check Repository Root for full License notice.
+
+osu!Hikaru, a fully independent osu!Lazer Private Server backend.
+
+Copyright (C) 2021 Hikaru Team <copyright@hikaru.pw>
--- loadMarkdown.js
+++ loadMarkdown.js
@@ -1,3 +1,7 @@
+// Licensed under GPL v3 - Check Repository Root for full License notice.
+// osu!Hikaru, a fully independent osu!Lazer Private Server backend.
+// Copyright (C) 2021 Hikaru Team <copyright@hikaru.pw>
+
 const FILENAME = location.pathname + "post.md";
 const set = (t) => (document.getElementById("content").innerHTML = t);
 const pull = fetch(FILENAME, {
